554af697745257105a187332cb8090c155d7e901
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-03-07 09:49:48 +0600

    finally installed all ngrx packages without error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +8,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapInterfaceComponent } from './components/map-interface/map-interface.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-import { environment } from 'src/environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent, SearchNavComponent, MapInterfaceComponent],
@@ -17,14 +19,12 @@ import { environment } from 'src/environments/environment';
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
-        // StoreModule.forRoot(reducers),
-        // StoreDevtoolsModule.instrument({
-        //     maxAge: 25, // Retains last 25 states
-        //     logOnly: environment.production, // Restrict extension to log-only mode
-        //     autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-        //     trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-        //     traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-        // }),
+        StoreModule.forRoot({}, {}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: environment.production,
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
